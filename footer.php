a921e99c2df514575df71e5d6922fa1fcf219f37
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2024-02-28 10:14:04 -0500

    more layout fixes

--- footer.php
+++ footer.php
@@ -2,21 +2,6 @@
 <link rel="stylesheet" href="assets/css/footer.css">
 
 <div class="footer">
-    <div class="row">
-        <div class="col-xs-4">
-        <!-- Bottom Menu -->
-        <div class="bottom-menu">
-            <?php foreach ($pages as $link): if(!$link['showInMenu']) continue; ?>
-                <a 
-                    class="link"
-                    onclick="scrollUpToSection('<?php echo $link['title']; ?>')"
-                    href="<?php echo $link['link']; ?>"
-                >
-                    <?php echo $link['title']; ?>
-                </a>
-            <?php endforeach; ?>
-        </div>
-    </div>
     <div class="copyright">
          © <?php echo date('Y'); ?> <?php echo $config['SITE_NAME']; ?>. All rights reserved.
     </div>
@@ -25,7 +10,7 @@
 <script>
 
 function scrollToSection(section) {
-    console.log(section)
+    toggleMobileMenu();
     const element = document.getElementById(section);
     element.scrollIntoView({behavior: "smooth"});
 }
